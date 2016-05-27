84125b2fba9824b6665c0ea0f39605a503c5895d
rasmus.lisborg <rasmus.lisborg@e2x.co.uk>
2016-05-27 11:20:08 +0100

    [ET,RL] Initial commit 6.0_R2.0

---
+++ hybris/bin/ext-worldpaytest/worldpaytests/src/com/worldpay/worldpaytests/constants/WorldpaytestsConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2013 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ * 
+ *  
+ */
+package com.worldpay.worldpaytests.constants;
+
+/**
+ * Global class for all Worldpaytests constants. You can add global constants for your extension into this class.
+ */
+public final class WorldpaytestsConstants extends GeneratedWorldpaytestsConstants
+{
+	public static final String EXTENSIONNAME = "worldpaytests";
+
+	private WorldpaytestsConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
